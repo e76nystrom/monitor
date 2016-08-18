887e55d7867b62950d5a40f544cbda0c01f1c468
Eric Nystrom <eric.nystrom@yahoo.com>
2016-08-18 18:33:39 -0400

    "load.sh Modified on 8/18/2016 at 6:33:37 PM"

---
+++ load.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#./avrdude -v -q -V -p32MX795F512L -Cavrdude.conf -D -cstk500v2 -b115200 -PCOM9 -Uflash:w:build-mega_pic32/ntp.hex:i
+
+~/.platformio/packages/tool-avrdude/avrdude \
+-D -v \
+-p atmega2560 \
+-C "C:\cygwin\home\Eric\.platformio\packages\tool-avrdude\avrdude.conf" \
+-c wiring \
+-b 115200 \
+-P COM14 \
+-U flash:w:.pioenvs/megaatmega2560/firmware.hex:i
+
+/cygdrive/c/DevSoftware/Putty/putty.exe -load COM4-19200
