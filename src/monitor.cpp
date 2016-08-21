3d0bade341fe6a7e306a286e6f29da619a03e118
Eric Nystrom <eric.nystrom@yahoo.com>
2016-08-21 05:57:32 -0400

    "src/monitor.cpp Modified on 8/21/2016 at 5:57:30 AM"

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -793,22 +793,16 @@ void cmdLoop()
      if (val == 0)
      {
       printf("turn relay off\n");
-      for (i = 0; i < 100; i++)
-      {
-       digitalWrite(DEHUM_OFF_PIN, HIGH);
-       delay(100);
-       digitalWrite(DEHUM_OFF_PIN, LOW);
-      }
+      digitalWrite(DEHUM_OFF_PIN, HIGH);
+      delay(100);
+      digitalWrite(DEHUM_OFF_PIN, LOW);
      }
      else
      {
       printf("turn relay on\n");
-      for (i = 0; i < 100; i++)
-      {
-       digitalWrite(DEHUM_ON_PIN, HIGH);
-       delay(100);
-       digitalWrite(DEHUM_ON_PIN, LOW);
-      }
+      digitalWrite(DEHUM_ON_PIN, HIGH);
+      delay(100);
+      digitalWrite(DEHUM_ON_PIN, LOW);
      }
     }
    }
