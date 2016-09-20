88e88e16a411e26c4e931f1f0d4732ba44fa30a2
Eric Nystrom <eric.nystrom@yahoo.com>
2016-09-20 15:13:52 -0400

    "src/millis.h Modified on 9/20/2016 at 3:13:50 PM"

---
+++ src/millis.h
@@ -0,0 +1,14 @@
+#if ARDUINO_ARCH_AVR
+extern volatile unsigned long timer0_millis
+#endif
+
+#if INT_MILLIS
+
+unsigned int intMillis()
+
+#define millis intMillis
+#define millisDef unsigned int
+
+#else
+#define millisDef unsigned long
+#endif
