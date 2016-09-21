e98ccb1891ca45dce52319cb5b0d3921357ec246
Eric Nystrom <eric.nystrom@yahoo.com>
2016-09-21 04:47:04 -0400

    "list.sh Modified on 9/21/2016 at 4:47:02 AM"

--- list.sh
+++ list.sh
@@ -18,16 +18,16 @@ avr-g++ \
 -DARDUINO_ARCH_AVR \
 -DARDUINO_AVR_MEGA2560 \
 -DARDUINO=10608 \
--IC:\cygwin\home\Eric\.platformio\packages\framework-arduinoavr\cores\arduino \
--IC:\cygwin\home\Eric\.platformio\packages\framework-arduinoavr\variants\mega \
--IC:\cygwin\home\Eric\.platformio\lib\Time_ID44 \
--IC:\cygwin\home\Eric\.platformio\packages\framework-arduinoavr\libraries\Wire\src \
--IC:\cygwin\home\Eric\.platformio\lib\DS3232RTC_ID78 \
--IC:\cygwin\home\Eric\.platformio\lib\OneWire_ID1 \
--IC:\cygwin\home\Eric\.platformio\packages\framework-arduinoavr\libraries\SoftwareSerial\src \
--IC:\cygwin\home\Eric\.platformio\lib\DallasTemperature_ID54 \
--IC:\cygwin\home\Eric\.platformio\lib\DHT_sensor_library_ID19 \
--IC:\cygwin\home\Eric\.platformio\packages\framework-arduinoavr\libraries\EEPROM\src \
+-IC:/cygwin/home/Eric/.platformio/packages/framework-arduinoavr/cores/arduino \
+-IC:/cygwin/home/Eric/.platformio/packages/framework-arduinoavr/variants/mega \
+-IC:/cygwin/home/Eric/.platformio/lib/Time_ID44 \
+-IC:/cygwin/home/Eric/.platformio/packages/framework-arduinoavr/libraries/Wire/src \
+-IC:/cygwin/home/Eric/.platformio/lib/DS3232RTC_ID78 \
+-IC:/cygwin/home/Eric/.platformio/lib/OneWire_ID1 \
+-IC:/cygwin/home/Eric/.platformio/packages/framework-arduinoavr/libraries/SoftwareSerial/src \
+-IC:/cygwin/home/Eric/.platformio/lib/DallasTemperature_ID54 \
+-IC:/cygwin/home/Eric/.platformio/lib/DHT_sensor_library_ID19 \
+-IC:/cygwin/home/Eric/.platformio/packages/framework-arduinoavr/libraries/EEPROM/src \
 -Isrc \
 -save-temps \
 -Wa,-adhln >monitor.lst \
