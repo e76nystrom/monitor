4faf3963acc0612438eb51277957f7a5f66284ee
Eric Nystrom <eric.nystrom@yahoo.com>
2021-08-14 16:54:12 -0400

    Remove sym links.

---
+++ rmlink.bat
@@ -0,0 +1,22 @@
+rm src\stm32f1adc.c 
+rm src\stm32f1dma.c 
+rm src\stm32f1gpio.c
+rm src\stm32f1i2c.c 
+rm src\stm32f1spi.c 
+rm src\stm32f1tim.c 
+rm src\current.cpp
+rm src\i2cx.cpp
+rm src\spix.cpp
+rm src\lcd.cpp
+rm src\stm32Info.cpp
+
+rm include\adc.h
+rm include\current.h
+rm include\dma.h
+rm include\gpio.h
+rm include\i2c.h
+rm include\main.h
+rm include\spi.h
+rm include\tim.h
+rm include\cyclectr.h
+rm include\stm32Info.h
