5d0506e49a459fab64b2d83fb27d95461434fe7e
Eric Nystrom <eric.nystrom@yahoo.com>
2021-08-31 03:09:02 -0400

    Enable watchdog timer for mega 2560.

--- src/wdt.h
+++ src/wdt.h
@@ -1,14 +1,11 @@
 #if defined(ARDUINO_ARCH_AVR)
 #define WDT 1
-#if WDT
-#include <avr/wdt.h>
-#define WDT_TO (WDTO_8S)
-#endif	/* WDT */
-#endif	/* ARDUINO_ARCH_AVR */
 
 #if defined(MEGA32)
-#define WDT 0
+#define WDT 1
 #endif	/* MEGA32 */
+
+#endif	/* ARDUINO_ARCH_AVR */
 
 #if defined(WIN32)
 #define WDT 0
@@ -17,6 +14,11 @@
 #if defined(ARDUINO_ARCH_STM32)
 #define WDT 0
 #endif	/* ARDUINO_ARCH_STM32) */
+
+#if WDT
+#include <avr/wdt.h>
+#define WDT_TO (WDTO_8S)
+#endif	/* WDT */
 
 #if !WDT
 #define WDT_TO
